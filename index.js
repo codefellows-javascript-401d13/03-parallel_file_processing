f83d5e7a55e09caaf713b9fccb5221a52570fc8a
Nikko Pisciotti <t.bird.13@hotmail.com>
2017-02-15 15:07:06 -0800

    got all files to read, they are outputting in order, however not sure if that's because of proper asynch management.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 'use-strict';
+
+const fileReader = require('./lib/file-reader.js');
+
+fileReader(`${__dirname}/data/one.txt`, function(err, data) {
+  if(err) throw err;
+  console.log('This is one.txt: ', data);
+
+  fileReader(`${__dirname}/data/two.txt`, function(err, data) {
+    if(err) throw err;
+    console.log('This is two.txt: ', data);
+
+    fileReader(`${__dirname}/data/three.txt`, function(err, data) {
+      if(err) throw err;
+      console.log('This is three.txt: ', data);
+    });
+  });
+});
