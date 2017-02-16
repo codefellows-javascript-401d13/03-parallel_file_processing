4aec979a48347f0eb00accfe1330de9b80a9241c
Dana Kulp <dkulp23@gmail.com>
2017-02-15 23:41:31 -0800

    Getting error message that done is getting called more than once.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-'use strict';
-
-// const dir = require('./lib/dir-reader.js').dataFiles;
-const reader = require('./lib/file-reader.js').readFiles;
-const handler = require('./lib/file-reader.js').handleData;
-
-reader(['one.txt', 'two.txt', 'three.txt'], handler);
+// 'use strict';
+//
+// const reader = require('./lib/file-reader.js').readFiles;
+//
+// reader(['one.txt', 'two.txt', 'three.txt']);
--- test/file-reader-test.js
+++ test/file-reader-test.js
@@ -6,7 +6,7 @@ const readFile = require('../lib/file-reader.js').readFiles;
 describe('File Reader Module', function() {
   describe('test false file path', function() {
     it('should return an error message', function(done) {
-      readFile(`${__dirname}/imaginary-file.txt`, function(err) {
+      readFile(['imaginary-file.txt'], function(err) {
         expect(err).to.be.an('error');
         done();
       });
@@ -14,8 +14,8 @@ describe('File Reader Module', function() {
   });
 
   describe('test actual file paths', function() {
-    it('should return the first 16 hex digits', function(done) {
-      readFile('one.txt', function(err, data) {
+    it('should return the first 8 hex digits', function(done) {
+      readFile(['one.txt', 'two.txt', 'three.txt'], function(err, data) {
         expect(err).to.equal(null);
         expect(data).to.be.a('string');
         done();
