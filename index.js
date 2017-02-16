e56ccdc7a29e3ffc00bf382638703f3b56a2a44c
caylazabel <caylazabel@gmail.com>
2017-02-15 20:10:34 -0800

    tests did work then broke again

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 'use strict';
 
 const fs = require('fs');
-const filesToBeRead =[`${__dirname}/data/one.txt`, `${__dirname}/data/two.txt`, `${__dirname}/data/three.txt`];
+const readFiles = require('./lib/file-reader.js')
+// const filesToBeRead =[`${__dirname}/data/one.txt`, `${__dirname}/data/two.txt`, `${__dirname}/data/three.txt`];
 
-module.exports = function readFiles(){
-  filesToBeRead.forEach(function(files){
-    fs.readFile(files, function(err, data){
-      if (err) throw err;
-      return data.toString('utf8', 0, 8);
-    });
-  });
-};
+
+//   filesToBeRead.forEach(function(files){
+//     fs.readFile(files, function(err, data){
+//       if (err) throw err;
+//       var contents = data.toString('hex', 0, 8);
+//       console.log(contents);
+//     });
+//   });
+// };
+
+readFiles.readEveryFile();
--- test/file-reader-test.js
+++ test/file-reader-test.js
@@ -3,6 +3,35 @@
 const expect = require('chai').expect;
 const fileReader = require('../lib/file-reader.js');
 
+describe('testing this file reader', function(){
+  describe('These hex values should be in order', function() {
+    it('the first one should be 3166696c65206e75', function(done){
+      fileReader.readOneFile(`${__dirname}/..data/one.txt`, function(err, data){
+        expect(err).to.equal(err);
+        expect(data).to.equal('3166696c65206e75');
+        done();
+      });
+    });
+
+    it('the second one should be 3366696c65206e75', function(done){
+      fileReader.readOneFile(`${__dirname}/..data/two.txt`, function(err, data){
+        expect(err).to.equal(err);
+        expect(data).to.equal('3366696c65206e75');
+        done();
+      });
+    });
+
+    it('the third one should be 3266696c65206e75', function(done){
+      fileReader.readOneFile(`${__dirname}/..data/three.txt`, function(err, data){
+        expect(err).to.equal(err);
+        expect(data).to.equal('3266696c65206e75');
+        done();
+      });
+    });
+  });
+});
+
+
 describe('File Reader Thing', function(){
   describe('with an improper file', function(){
     it('should return an error', function(done){
@@ -12,35 +41,4 @@ describe('File Reader Thing', function(){
       });
     });
   });
-  describe('with a proper file path', function() {
-    it('should return the contents of file one', function(done) {
-      fileReader(`${__dirname}/../data/one.txt`, function(err, data) {
-        expect(err).to.equal(null);
-        expect(data).to.be.a('string');
-        expect(data).to.equal('file number one\n');
-        done();
-      });
-    });
-  });
-  describe('with a proper file path', function() {
-    it('should return the contents of file two', function(done) {
-      fileReader(`${__dirname}/../data/two.txt`, function(err, data) {
-        expect(err).to.equal(null);
-        expect(data).to.be.a('string');
-        expect(data).to.equal('file number two\n');
-        done();
-      });
-    });
-  });
-  describe('with a proper file path', function() {
-    it('should return the contents of file three', function(done) {
-      fileReader(`${__dirname}/../data/three.txt`, function(err, data) {
-        expect(err).to.equal(null);
-        expect(data).to.be.a('string');
-        expect(data).to.equal('file number three\n');
-        done();
-      });
-    });
-  });
-
 });
