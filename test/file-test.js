c704f5b98f5a131e6affbce77f406cb2ab7a1b3a
Gary Lundgren <builtbygl@gmail.com>
2017-02-16 20:47:34 -0800

    added some tests and fixed filereader

--- test/file-test.js
+++ test/file-test.js
@@ -4,17 +4,31 @@ const expect = require('chai').expect;
 const fileReader = require('../lib/file-reader.js');
 
 describe('File reading module', function() {
-  describe('with an improper path', function() {
-    it('should return with an error', function(done) {
-      fileReader(`${__dirname}/../not-a-file.text`, function(err) {
-        expect(err).to.be.an('error');
-        done();
+  describe('with improper paths', function() {
+    it('should return with an error', function() {
+      fileReader(`${__dirname}/dont-exit.text`, function() {
+        expect(err).to.equal(true);
       });
     });
   });
-  describe('with a proper path for file one', function() {
-    it('should return the file for file one.txt', function() {
-      // TODO: make a test
-    })
+  describe('with a proper paths', function() {
+    before((done) => {
+      this.paths = [
+        `${__dirname}/../data/one.txt`,
+        `${__dirname}/../data/two.txt`,
+        `${__dirname}/../data/three.txt`,
+      ];
+    });
+    it('should have the correct order of hex strings read in', (done) => {
+      var expectedResult = [];
+      readFile(this.paths, function(err, data) {
+        expect(err).to.equal(null);
+        expect(data[0]).to.equal(expectedResult[0]);
+        expect(data[1]).to.equal(expectedResult[1]);
+        expect(data[2]).to.equal(expectedResult[2]);
+        console.log('data', data);
+        done();
+      });
+    });
   });
 });
