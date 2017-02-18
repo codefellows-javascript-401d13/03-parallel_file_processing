9dca695c9a2a8b19b01e21442e403fe87608645f
Nikko Pisciotti <t.bird.13@hotmail.com>
2017-02-18 15:33:48 -0800

    refactored code similar to how we completed in class with lexical arrow funcs and testing results against an array of sequential values

--- test/file-reader-test.js
+++ test/file-reader-test.js
@@ -3,26 +3,28 @@
 const expect = require('chai').expect;
 const fileReader = require('../lib/file-reader.js');
 
+
+
+
+
 describe('File Reader', function() {
   describe('Hex values should display in order.', function() {
-    it('first output should be: 5468697320697320', function(done) {
-      fileReader.readSingleFile(`${__dirname}/../data/one.txt`, function(err, data) {
-        expect(err).to.equal(null);
-        expect(data).to.equal('5468697320697320');
-        done();
-      });
+    before((done) => {
+      this.paths = [
+        `${__dirname}/../data/one.txt`,
+        `${__dirname}/../data/two.txt`,
+        `${__dirname}/../data/three.txt`
+      ];
+      done();
     });
-    it('second output should be: 6c61682064656520', function(done) {
-      fileReader.readSingleFile(`${__dirname}/../data/two.txt`, function(err, data) {
+    it('should test pathArray and correctly log each result', (done) => {
+      var expectedResults = ['5468697320697320', '6c61682064656520', '4c6f72656d206970'];
+      fileReader.readAllFiles(this.paths, function(err, data) {
         expect(err).to.equal(null);
-        expect(data).to.equal('6c61682064656520');
-        done();
-      });
-    });
-    it('third output should be: 4c6f72656d206970', function(done) {
-      fileReader.readSingleFile(`${__dirname}/../data/three.txt`, function(err, data) {
-        expect(err).to.equal(null);
-        expect(data).to.equal('4c6f72656d206970');
+        expect(data[0]).to.equal(expectedResults[0]);
+        expect(data[1]).to.equal(expectedResults[1]);
+        expect(data[2]).to.equal(expectedResults[2]);
+        console.log('data', data);
         done();
       });
     });
