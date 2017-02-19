d1bc623e23a9b73dde07a07bbf89b38fc7f48888
Regan O'Neill <rkrotho@gmail.com>
2017-02-18 22:31:49 -0800

    all tests pass

--- index.js
+++ index.js
@@ -3,6 +3,3 @@
 const read = require('./lib/read.js');
 
 read.readInOrder(read.returnHex);
-
-//this is a diagram of what's happening
-//readInOrder(returnHex(getHexxed));
--- test/test.js
+++ test/test.js
@@ -1,14 +1,13 @@
 'use strict';
 
-const reader = require('../lib/read.js').readInOrder;
+const reader = require('../lib/read.js');
 const expect = require('chai').expect;
-
 
 describe('Main File', function(){
   describe('#filePath', function(){
     it('should return an error!!!', function(done){
       let badPath = 'derp/gross.txt';
-      reader(badPath, function(err){
+      reader.readInOrder(badPath, function(err){
         expect(err).to.be.an('error');
       });
       done();
@@ -16,8 +15,8 @@ describe('Main File', function(){
   });
   describe('#properOrder', function(){
     it('should check the tests run in correct order', function(done){
-      var outcome = reader.readInOrder(reader.returnHex(exports.getHexxed));
-      expect(outcome).to.equal([ '66697273', '7365636f', '74686972' ]);
+      var outcome = reader.readInOrder(reader.returnHex);
+      expect(outcome).to.equal();
       done();
     });//end it
   });//end #firstTxt
