8740877b7765e5f68dfd2f4f74079afdeb0fd12e
Jeremiah <jeremiaht.walters@gmail.com>
2017-02-20 09:37:41 -0800

    resubmit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const fileReader = require('./lib/file-reader.js');
+const readFile = require('./lib/read-file.js');
 
-fileReader([`${__dirname}/data/one.txt`, `${__dirname}/data/two.txt`, `${__dirname}/data/three.txt`], function (err, result){
+readFile([`${__dirname}/data/one.txt`, `${__dirname}/data/two.txt`, `${__dirname}/data/three.txt`], function (err, result){
   if (err) throw err;
   console.log(result);
 });
