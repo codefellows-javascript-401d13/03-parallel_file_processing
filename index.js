6c7695a6667671e445a0b88a82caeda4393df242
Dana Kulp <dkulp23@gmail.com>
2017-02-26 17:56:55 -0800

    Completed refactor and tests. Halved third file to test async return.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const reader = require('./lib/file-reader.js').readFiles;
+const reader = require('./lib/file-reader.js');
 
 reader(['one.txt', 'two.txt', 'three.txt'], function(err, data) {
 	if (err) console.error(err);
--- test/file-reader-test.js
+++ test/file-reader-test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const expect = require('chai').expect;
-const readFile = require('../lib/file-reader.js').readFiles;
+const readFile = require('../lib/file-reader.js');
 
 describe('File Reader Module', function() {
   describe('test false file path', function() {
@@ -14,10 +14,17 @@ describe('File Reader Module', function() {
   });
 
   describe('test actual file paths', function() {
-    it('should return the first 8 hex digits', function(done) {
-      readFile(['one.txt', 'two.txt', 'three.txt'], function(err, data) {
+    let files = ['one.txt', 'two.txt', 'three.txt'];
+    let expected = ['456e616d656c2070', '536c6f772d636172', '53756363756c656e'];
+    it('should return the first 8 hex digits in order', function(done) {
+      readFile(files, function(err, data) {
         expect(err).to.equal(null);
-        expect(data).to.be.a('string');
+        expect(data[0]).to.be.a('string');
+        expect(data[0]).to.equal(expected[0]);
+        expect(data[1]).to.be.a('string');
+        expect(data[1]).to.equal(expected[1]);
+        expect(data[2]).to.be.a('string');
+        expect(data[2]).to.equal(expected[2]);
         done();
       });
     });
