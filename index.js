e081ea898c11eebe240a99a054ef51defe584a7e
Dana Kulp <dkulp23@gmail.com>
2017-02-26 17:25:42 -0800

    Refactoring file reader and added gulp file.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-// 'use strict';
-//
-// const reader = require('./lib/file-reader.js').readFiles;
-//
-// reader(['one.txt', 'two.txt', 'three.txt']);
+'use strict';
+
+const reader = require('./lib/file-reader.js').readFiles;
+
+reader(['one.txt', 'two.txt', 'three.txt'], function(err, data) {
+	if (err) console.error(err);
+	console.log('index.js', data);
+});
