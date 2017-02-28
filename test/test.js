cd44a1dd239648de195b22e20acb982ec44df60a
reganoneill <rkrotho@gmail.com>
2017-02-28 00:05:16 -0800

    built meaningful tests and cleaned up wode

--- test/test.js
+++ test/test.js
@@ -4,20 +4,35 @@ const reader = require('../lib/read.js');
 const expect = require('chai').expect;
 
 describe('Main File', function(){
-  describe('#filePath', function(){
-    it('should return an error!!!', function(done){
-      let badPath = 'derp/gross.txt';
+
+require('../index.js');
+
+  describe('#wrongFilePath', function(){
+    it('should return an error', function(done){
+      let badPath = 'derp/wrong.txt';
       reader.readInOrder(badPath, function(err){
         expect(err).to.be.an('error');
       });
       done();
     });
   });
-  describe('#properOrder', function(){
-    it('should check the tests run in correct order', function(done){
-      var outcome = reader.readInOrder(reader.returnHex);
-      expect(outcome).to.equal();
+
+  describe('#properAmountOfFiles', function(){
+    it('check number of files in array', function(done){
+      expect(reader.dataHolder.length).to.equal(3);
       done();
-    });//end it
-  });//end #firstTxt
+    });
+  });
+
+  describe('#filesMatchUp', function(){
+    it('check file order', function(done){
+      expect(reader.fileHolder[0]).to.equal('one.txt');
+      expect(reader.fileHolder[1]).to.equal('two.txt');
+      expect(reader.fileHolder[2]).to.equal('three.txt');
+      done();
+    });
+  });
+
+
+
 });
