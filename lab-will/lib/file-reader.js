cdc2400ba03fd31a44647235e9617880398d64d4
william-skelton <william.f.skelton@outlook.com>
2017-03-05 11:20:43 -0800

    refactored

--- lab-will/lib/file-reader.js
+++ lab-will/lib/file-reader.js
@@ -2,24 +2,22 @@
 
 const fs = require('fs');
 
-const files = ['one', 'two', 'three'];
+const readFileHelper = module.exports = function(pathArray, callback) {
+  var result = [];
 
-var readFiles = function () {
-  var test = [];
-  for (var k = 0; k < files.length; k++) {
-    fs.readFile(`${__dirname}/../data/${files[k]}.txt`, function(err, data) {
-      if(err) throw err;
-      var hexArray = [];
-      for (var i = 0; i < 8; i++) {
-        var str = data.readUInt8(i);
-        // console.log(str.toString(16));
-        hexArray[i] = str.toString(16);
-      }
-      console.log(hexArray);
-      console.log('==================================================');
+  fs.readFile(pathArray[0], function(err, data) {
+    if(err) return callback(err);
+    result.push(data.toString('hex', 0, 8));
 
-    })
-  }
-}
+    fs.readFile(pathArray[1], function(err, data) {
+      if(err) return callback(err);
+      result.push(data.toString('hex', 0, 8));
 
-readFiles();
+      fs.readFile(pathArray[2], function(err, data) {
+        if(err) return callback(err);
+        result.push(data.toString('hex', 0, 8));
+        callback(null, result);
+      });
+    });
+  });
+};
