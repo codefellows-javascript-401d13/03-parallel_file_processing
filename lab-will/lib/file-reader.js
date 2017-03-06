d72ff21fb389e18fd9ce17017adf6d17eab43352
william-skelton <william.f.skelton@outlook.com>
2017-03-05 19:29:59 -0800

    command line call is now working

--- lab-will/lib/file-reader.js
+++ lab-will/lib/file-reader.js
@@ -16,8 +16,16 @@ const readFileHelper = module.exports = function(pathArray, callback) {
       fs.readFile(pathArray[2], function(err, data) {
         if(err) return callback(err);
         result.push(data.toString('hex', 0, 8));
+        console.log('result: ', result);
         callback(null, result);
       });
     });
   });
 };
+
+const paths = [
+      `${__dirname}/../data/one.txt`,
+      `${__dirname}/../data/two.txt`,
+      `${__dirname}/../data/three.txt`
+    ];
+readFileHelper(paths, (err) => console.log('err:', err) );
