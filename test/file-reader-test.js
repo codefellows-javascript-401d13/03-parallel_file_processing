f0c25c492d9c99abd6480c520d7b00b9ae837b40
sueanyv <valenciasue@hotmail.com>
2017-03-12 23:42:16 -0700

    fixed and made test

--- test/file-reader-test.js
+++ test/file-reader-test.js
@@ -1,35 +1,34 @@
 'use strict';
 
-const fs = require('fs');
 const expect = require('chai').expect;
-const fileReader = require('../lib/file-reader.js');
+const readFile = require('../lib/file-reader.js');
 
-describe('Reading all three files', function(){
-
-
-  it('should rad the files in order, and provide the first 8 bytes of each file', function(done){
-    fileReader(`${__dirname}/../data/one.txt`, function(err, data){
-      expect(err).to.equal(err);
-      expect(data).to.equal('48656c6c6f207468');
-      expect(data).to.have.length.above(8);
-      fileReader(`${__dirname}/../data/two.txt`, function(err, data){
-        expect(err).to.equal(err);
-        expect(data).to.equal('48656c6c6f207468');
-        expect(data).to.have.length.above(8);
-        fileReader(`${__dirname}/../data/three.txt`, function(err, data){
-          expect(err).to.equal(err);
-          expect(data).to.equal('48656c6c6f207468');
-          expect(data).to.have.length.above(8);
-          done();
-        });
+describe('Read File Helper Module', () =>{
+  describe('with bad file paths', () =>{
+    it('should return an error', () =>{
+      readFile([`${__dirname}/.txt`], (err) =>{
       });
     });
   });
-  it('File not found.', function(done) {
-    fileReader('', function(err, data) {
-      expect(err).to.exist;
-      expect(data).to.not.exist;
+describe('with good file paths', () =>{
+  before((done) => {
+    this.paths = [
+      `${__dirname}/../data/one.txt`,
+      `${__dirname}/../data/two.txt`,
+      `${__dirname}/../data/three.txt`
+    ];
+    done();
+  });
+  it('should have correct order of hex strings', (done) => {
+    var dataNum = ['48656c6c6f207468', '48656c6c6f207468', '48656c6c6f207468'];
+    readFile(this.paths, (err, data) =>{
+      expect(err).to.equal(null);
+      expect(data[0]).to.equal(dataNum[0]);
+      expect(data[1]).to.equal(dataNum[1]);
+      expect(data[2]).to.equal(dataNum[2]);
+      console.log('data', data);
       done();
     });
   });
+});
 });
